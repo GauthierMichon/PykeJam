2716cb1d24d66fbc5bb06571887891759e46a8db
BenjaminChancerel <benjamin.chancerel@ynov.com>
2022-03-16 11:23:39 +0100

    dresseur

--- classes/dresseur.py
+++ classes/dresseur.py
@@ -1,4 +1,4 @@
-class Pokemon():
+class Dresseur():
     def __init__(self, name, pokemon1, pokemon2, pokemon3, pokemon4, pokemon5, pokemon6):
         self.name       = name
         self.pokemon1   = pokemon1
