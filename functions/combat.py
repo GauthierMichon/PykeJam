9bb2187c9552b48aaee903a3a4f56f4826924009
GMICHON <gauthier.michon@ynov.com>
2022-04-13 09:34:30 +0200

    switch + début tour adverse

--- functions/combat.py
+++ functions/combat.py
@@ -1,7 +1,8 @@
 from classes.terrain import Terrain
-from functions.change_pokemon import Switch
 from functions.choose_action import chooseAction
+from functions.choose_random_num import rand
 from functions.fight import fight
+from functions.switch import Switch
 
 
 def combat(player, adversaire) :
@@ -12,14 +13,33 @@ def combat(player, adversaire) :
 
     terrain = Terrain()
 
-    while True :
+    boolean = True
+    while boolean :
         if player.actionOblig == None :
             action, actionNum = chooseAction(player, adversaire, player.pokemons[pokemonActualPlayerNumber], adversaire.pokemons[pokemonActualAdversNumber], player, pokemonActualPlayerNumber)
         else : 
             action = player.actionOblig[0]
             actionNum = player.actionOblig[1]
-        
+
         action_adversaire, actionNum_adversaire = 1, 1
+
+        if action == 2 or action == 3 :
+            print("player begin")
+        elif action_adversaire == 2 or action_adversaire == 3 :
+            print("advers begin")
+        elif player.pokemons[pokemonActualPlayerNumber].Attaques[actionNum].priorityLevel > adversaire.pokemons[pokemonActualAdversNumber].Attaques[actionNum_adversaire].priorityLevel :
+            print("player begin")
+        elif player.pokemons[pokemonActualPlayerNumber].Attaques[actionNum].priorityLevel < adversaire.pokemons[pokemonActualAdversNumber].Attaques[actionNum_adversaire].priorityLevel :
+            print("advers begin")
+        elif player.pokemons[pokemonActualPlayerNumber].Speed > adversaire.pokemons[pokemonActualAdversNumber].Speed :
+            print("player begin")
+        elif player.pokemons[pokemonActualPlayerNumber].Speed < adversaire.pokemons[pokemonActualAdversNumber].Speed :
+            print("advers begin")
+        else :
+            if rand(1, 2) == 1 :
+                print("player begin")
+            else :
+                print("advers begin")
 
         if action == 1 :
             #if player.pokemons[pokemonActualPlayerNumber].Speed > adversaire.pokemons[pokemonActualAdversNumber].Speed :
@@ -29,4 +49,11 @@ def combat(player, adversaire) :
                 
         elif action == 2 :
             print("{} a été envoyé".format(player.pokemons[actionNum].name))
+            player = Switch(player, pokemonActualPlayerNumber)
+            pokemonActualPlayerNumber = actionNum
+
+        elif action == 3 :
+            print("item mais pas encore implémenté")
+
+    
         
---
+++ functions/switch.py
@@ -0,0 +1,25 @@
+def Switch(dresseur, currentPokemonNum) :
+    dresseur.pokemons[currentPokemonNum].confusion      = False
+    dresseur.pokemons[currentPokemonNum].confusionNum   = None
+    dresseur.pokemons[currentPokemonNum].afraid         = False
+    dresseur.pokemons[currentPokemonNum].abri           = False
+    dresseur.pokemons[currentPokemonNum].clone          = False
+    dresseur.pokemons[currentPokemonNum].clonePV        = None
+    dresseur.pokemons[currentPokemonNum].maudit         = False
+    dresseur.pokemons[currentPokemonNum].requiem        = False
+    dresseur.pokemons[currentPokemonNum].requiemNum     = None
+    dresseur.pokemons[currentPokemonNum].siphon         = False
+    dresseur.pokemons[currentPokemonNum].siphonNum      = None
+    dresseur.pokemons[currentPokemonNum].AttBuff        = 0
+    dresseur.pokemons[currentPokemonNum].Att            = dresseur.pokemons[currentPokemonNum].AttInit
+    dresseur.pokemons[currentPokemonNum].DefBuff        = 0
+    dresseur.pokemons[currentPokemonNum].Def            = dresseur.pokemons[currentPokemonNum].DefInit
+    dresseur.pokemons[currentPokemonNum].AttSpeBuff     = 0
+    dresseur.pokemons[currentPokemonNum].AttSpe         = dresseur.pokemons[currentPokemonNum].AttSpeInit
+    dresseur.pokemons[currentPokemonNum].DefSpeBuff     = 0
+    dresseur.pokemons[currentPokemonNum].DefSpe         = dresseur.pokemons[currentPokemonNum].DefSpeInit
+    dresseur.pokemons[currentPokemonNum].SpeedBuff      = 0
+    dresseur.pokemons[currentPokemonNum].Speed          = dresseur.pokemons[currentPokemonNum].SpeedInit
+    dresseur.pokemons[currentPokemonNum].accuracy       = dresseur.pokemons[currentPokemonNum].accuracy
+
+    return dresseur
